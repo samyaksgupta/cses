238e8f955e2b8625fd57bd9c7698a3a4c6abfe3c
samyaksgupta <127541694+samyaksgupta@users.noreply.github.com>
2025-05-23 17:00:03 +0530

    Create repetitions.cpp

---
+++ repetitions.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+//cses #3
+int main() {
+    string n;
+    cin >> n;
+    int ans = 1, temp = 1;
+    for (int i = 1 ; i < n.size() ; i++) {
+        if (n[i] == n[i-1]) {
+            ++temp;
+        } else {
+            ans = max(ans, temp);
+            temp = 1;
+ 
+        }
+ 
+    }
+    ans = max(ans, temp);
+    cout << ans;
+    return 0;
+ 
+    
+}
